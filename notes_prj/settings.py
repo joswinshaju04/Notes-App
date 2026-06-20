5f6d3c11fed149bd6c24a5aa0f274be4c7575600
JOSWIN SHAJU <joswinshaju2028@cs.sjcetpalai.ac.in>
2026-06-20 11:14:22 +0530

    pushing

--- notes_prj/settings.py
+++ notes_prj/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'notes_prj.urls'
@@ -116,3 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'notes_app' / "static",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
